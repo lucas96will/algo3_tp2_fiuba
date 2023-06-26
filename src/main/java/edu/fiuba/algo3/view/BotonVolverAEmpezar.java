f414e16d6ddaf55017832d7bfe732807a80022a0
Carlos Orqueda <corqueda.knh@veritran.com>
2023-06-25 21:27:26 -0300

    MVC pantalla final, mas maqueta de boton volver a empezar CAO

--- src/main/java/edu/fiuba/algo3/view/BotonVolverAEmpezar.java
+++ src/main/java/edu/fiuba/algo3/view/BotonVolverAEmpezar.java
@@ -3,7 +3,7 @@ package edu.fiuba.algo3.view;
 import edu.fiuba.algo3.App;
 import edu.fiuba.algo3.controllers.ControladorDeBoton;
 import edu.fiuba.algo3.controllers.ControladorDeIngresarNombre;
-import edu.fiuba.algo3.controllers.ControladorPantallaInicio;
+import edu.fiuba.algo3.controllers.ControladorPantallaFinal;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
@@ -11,22 +11,22 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
-public class BotonIngresarNombre extends Button {
-    public static Button fijarBotonEmpezar(ControladorDeIngresarNombre controlador){
+public class BotonVolverAEmpezar extends Button {
+    public static Button fijarBotonVolverAEmpezar(ControladorPantallaFinal controlador, String mensaje){
         ControladorDeBoton controladorDeBoton = new ControladorDeBoton();
         controladorDeBoton.initialize((App.class.getResource("/fxml/boton.fxml")),null);
-        Button boton = controladorDeBoton.obtenerBoton("", controlador.empezar());
+        Button boton = controladorDeBoton.obtenerBoton("", controlador.volverAEmpezar());
         boton.setAlignment(Pos.CENTER);
         boton.setContentDisplay(ContentDisplay.CENTER);
         boton.setMnemonicParsing(false);
         boton.setStyle("-fx-background-radius: 10; -fx-background-color: rgb(255, 206, 91); -fx-padding: 20px 100px 18px 100px;");
-        boton.setText("Empezar");
+        boton.setText(mensaje);
         boton.setTextAlignment(TextAlignment.CENTER);
         boton.setTextFill(Color.WHITE);
-        boton.setTranslateX(420);
+        boton.setTranslateX(350);
         boton.setTranslateY(550);
         boton.setFont(new Font(36));
-        boton.setId("empezar");
+        boton.setId(mensaje);
         return boton;
     }
 }
